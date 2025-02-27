97ffd437002917224789e9453070df90c69f457b
adevair.junior <profadevairvitorio@gmail.com>
2025-02-27 09:31:46 -0300

    feat: Suitability for HTML escape

--- src/music/music.service.ts
+++ src/music/music.service.ts
@@ -5,13 +5,19 @@ import {MajorScale} from "./models/major-scale.model";
 
 @Injectable()
 export class MusicService {
+    private normalizeRoot(root: string): string {
+        return root.replace('sharp', '#');
+    }
+
     getMajorScale(root: string): ScaleInterface {
-        const scale = new MajorScale(root);
+        const normalizedRoot = this.normalizeRoot(root);
+        const scale = new MajorScale(normalizedRoot);
         return scale.getScale();
     }
 
     getMinorScale(root: string): ScaleInterface {
-        const scale = new MinorScale(root);
+        const normalizedRoot = this.normalizeRoot(root);
+        const scale = new MinorScale(normalizedRoot);
         return scale.getScale();
     }
 }
